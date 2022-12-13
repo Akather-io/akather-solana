803d5e40ba46b25bcf98686053de84bd924ff48f
doquoctin94 <doquoctin94@gmail.com>
2022-12-13 18:30:29 +0700

    update stake token ui

---
+++ src/app/stake/page.tsx
@@ -0,0 +1,10 @@
+import StakeToken from "../../components/StakeToken/StakeToken";
+import Container from "../../components/_UI/Container";
+
+export default function StakePage() {
+  return (
+    <Container>
+      <StakeToken />
+    </Container>
+  );
+}
